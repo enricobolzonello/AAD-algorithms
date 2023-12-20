78e55dab3c739453b49f12e548d65240d9570465
Enrico Bolzonello <enribolzonello@gmail.com>
2023-12-20 14:10:24 +0100

    Extended Euclid

--- Euclid.py
+++ Euclid.py
@@ -1,7 +1,18 @@
+import math
+
 def Euclid(a,b):
     if b==0:
         return a
     return Euclid(b, a%b)
 
+def ExtendedEuclid(a,b):
+    if b==0:
+	    return (a, (1,0))
+    temp = ExtendedEuclid(b,a%b)
+    x = temp[1][0]
+    y = temp[1][1]
+    return (temp[0], (y, x - math.floor(a/b)*y))
+
 if __name__ == "__main__":
     print(Euclid(210, 45))
+    print(ExtendedEuclid(24, 16))
